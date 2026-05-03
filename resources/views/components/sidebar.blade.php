01d5bf804a9f5c66bcad84400276adcc5931bc1a
vivireginaa28 <vivireginaa28@gmail.com>
2026-05-03 19:03:56 +0700

    membuat components sidebar

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,20 @@
+<aside class="w-72 bg-white border-r border-gray-200 flex flex-col shadow-sm">
+
+    <div class="p-8">
+        <h1 class="text-3xl font-bold text-blue-600 italic">Rapor.id</h1>
+    </div>
+
+    <nav class="flex-1 px-4 space-y-2">
+
+        <a href="#" class="block px-4 py-3 rounded-xl hover:bg-gray-100">Dashboard</a>
+
+        <a href="#" class="block px-4 py-3 bg-blue-600 text-white rounded-xl font-bold">
+            Guru
+        </a>
+
+        <a href="#" class="block px-4 py-3 rounded-xl hover:bg-gray-100">Siswa</a>
+        <a href="#" class="block px-4 py-3 rounded-xl hover:bg-gray-100">Mapel</a>
+
+    </nav>
+
+</aside>
