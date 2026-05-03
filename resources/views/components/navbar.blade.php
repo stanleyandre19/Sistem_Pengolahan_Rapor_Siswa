15ab411ba18668977f4026dd1c435171a8ae3c84
vivireginaa28 <vivireginaa28@gmail.com>
2026-05-03 19:10:34 +0700

    membuat navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,23 @@
+<header class="bg-white border-b border-gray-200 px-10 py-4 flex justify-between items-center shadow-sm">
+
+    <!-- JUDUL SISTEM -->
+    <h1 class="text-lg font-bold text-gray-700">
+        Sistem Pengelolaan Rapor Siswa
+    </h1>
+
+    <!-- USER -->
+    <div class="flex items-center gap-4">
+
+        <span class="text-sm font-medium text-gray-600">
+            👤 Admin
+        </span>
+
+        <!-- LOGOUT -->
+        <a href="/logout"
+           class="bg-red-500 text-white px-4 py-2 rounded-lg text-sm hover:bg-red-600">
+            Logout
+        </a>
+
+    </div>
+
+</header>
