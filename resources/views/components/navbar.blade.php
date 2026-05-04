9eba379def9b4d736c1dd71c1922a79a55912a2c
ristachristynainggolan-cpu <ristachristynainggolan@gmail.com>
2026-05-04 08:18:54 +0700

    Update components/navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,19 +1,23 @@
 <header class="bg-white border-b border-gray-200 px-10 py-4 flex justify-between items-center shadow-sm">
 
-    <!-- JUDUL SISTEM -->
     <h1 class="text-lg font-bold text-gray-700">
         Sistem Pengelolaan Rapor Siswa
     </h1>
 
-    <!-- USER -->
-    <div class="flex items-center gap-4">
+    <div class="flex items-center gap-6">
 
+        <!-- MENU -->
+        <a href="/home" class="text-sm hover:text-blue-600">Home</a>
+        <a href="/about" class="text-sm hover:text-blue-600">About</a>
+        <a href="/dashboard" class="text-sm hover:text-blue-600">Dashboard</a>
+
+        <!-- USER -->
         <span class="text-sm font-medium text-gray-600">
             👤 Admin
         </span>
 
-        <!-- LOGOUT -->
-        <a href="/logout"
+        <!-- LOGOUT (AMAN) -->
+        <a href="/login"
            class="bg-red-500 text-white px-4 py-2 rounded-lg text-sm hover:bg-red-600">
             Logout
         </a>
