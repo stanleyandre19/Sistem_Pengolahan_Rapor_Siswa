9978cf371afcd7ee0082943b5b7779799b37b078
vivireginaa28 <vivireginaa28@gmail.com>
2026-06-20 11:12:40 +0700

    update tambah_siswa.blade.php

--- resources/views/pages/tambah_siswa.blade.php
+++ resources/views/pages/tambah_siswa.blade.php
@@ -4,7 +4,7 @@
 
 <h1 class="text-xl font-bold mb-4">Tambah Siswa</h1>
 
-<form action="/siswa" method="POST"
+<form action="/siswa" method="POST" enctype="multipart/form-data"
       class="bg-white p-6 rounded shadow space-y-4">
     @csrf
 
@@ -15,6 +15,10 @@
            class="w-full border p-2 rounded">
 
     <input type="text" name="kelas" placeholder="Kelas"
+           class="w-full border p-2 rounded">
+
+    <!-- FOTO -->
+    <input type="file" name="foto"
            class="w-full border p-2 rounded">
 
     <button class="bg-blue-600 text-white px-4 py-2 rounded">
